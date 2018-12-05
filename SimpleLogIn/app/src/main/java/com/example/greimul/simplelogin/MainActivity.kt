7a6e456438ce290191fad103a851859726c12eac
GreimuL <yoseib1998@naver.com>
2018-12-05 20:35:06 +0900

    Fix simplelogin save load

--- SimpleLogIn/app/src/main/java/com/example/greimul/simplelogin/MainActivity.kt
+++ SimpleLogIn/app/src/main/java/com/example/greimul/simplelogin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.audiofx.EnvironmentalReverb
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -15,54 +16,76 @@ import android.text.method.PasswordTransformationMethod
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.*
+import java.lang.Exception
 import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
+    var permission = Manifest.permission.WRITE_EXTERNAL_STORAGE
+
     var pwshow:Boolean = false
     var accountData:List<String> = ArrayList<String>()
+
+    val path = Environment.getExternalStorageDirectory()
+    val directory = File(path, "SimpleLogin")
+    val getfile = File(directory, "AccountData.txt")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
-        dataArrange()
-        logIn.setOnClickListener{
+        loadAccount()
+        logIn.setOnClickListener {
 
         }
-        signUp.setOnClickListener{
-            val intent = Intent(this,SignUpActivity::class.java)
+        signUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
             finish()
         }
-        checkBox.setOnClickListener{
-            if(pwshow == true){
+
+        checkBox.setOnClickListener {
+            if (pwshow == true) {
                 pwshow = false
                 pwInput.transformationMethod = PasswordTransformationMethod.getInstance()
-            }
-            else{
+            } else {
                 pwshow = true
                 pwInput.transformationMethod = HideReturnsTransformationMethod()
             }
         }
     }
     fun init(){
-        var os = openFileOutput("AccountData.txt", Context.MODE_PRIVATE)
-        os.write("asdf".toByteArray())
-        os.close()
+        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
+            return;
+        }
+        var ck:Int = checkCallingOrSelfPermission(permission)
+        if(ck== PackageManager.PERMISSION_DENIED){
+            requestPermissions(arrayOf(permission),0)
+        }
+        try {
+            directory.mkdirs()
+            if (!getfile.exists()) {
+                FileOutputStream(getfile)
+            }
+        }
+        catch(e:Exception){
+            Toast.makeText(this,"Initial setting failed",Toast.LENGTH_SHORT)
+        }
     }
-    fun dataArrange(){
-        val directory = File(this.filesDir,"SimpleLogin")
-        val file = File(directory,"AccountData.txt")
-        file.createNewFile()
-        accountData = file.readLines()
+    fun loadAccount(){
+        try {
+            accountData = getfile.readLines()
+            Toast.makeText(this, "AccountData load Success", Toast.LENGTH_LONG).show()
+        }
+        catch(e:Exception){
+            Toast.makeText(this, "AccountData load Failed\nPlease allow permission\nAnd restart app", Toast.LENGTH_LONG).show()
+        }
     }
+
     fun File.readLines():List<String>{
-        val result = ArrayList<String>()
-        BufferedReader(InputStreamReader(FileInputStream(this))).forEachLine({
-            result.add(it)
-        })
-        return result
+        var tmp = ArrayList<String>()
+        BufferedReader(InputStreamReader(FileInputStream(this))).forEachLine ({ tmp.add(it) })
+        return tmp
     }
 }
--- SimpleLogIn/app/src/main/java/com/example/greimul/simplelogin/SignUpActivity.kt
+++ SimpleLogIn/app/src/main/java/com/example/greimul/simplelogin/SignUpActivity.kt
@@ -2,23 +2,59 @@ package com.example.greimul.simplelogin
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Environment
 import android.support.v7.app.AppCompatActivity
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_signup.*
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.lang.Exception
 
 class SignUpActivity:AppCompatActivity() {
 
+    var accountData:List<String> = ArrayList<String>()
+
     var pwshow:Boolean = false
+    var idCheck:Boolean = false
+
+    val path = Environment.getExternalStorageDirectory()
+    val directory = File(path, "SimpleLogin")
+    val getfile = File(directory, "AccountData.txt")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        accountData = getfile.readLines()
         checkDupli.setOnClickListener{
-
+            var checkD:Boolean = true
+            accountData.forEach{
+                if(it==signUpID.text.toString()){
+                    checkD = false
+                }
+            }
+            if(checkD){
+                idCheck = true
+                Toast.makeText(this,"Accept",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"ID already exists",Toast.LENGTH_SHORT).show()
+            }
         }
         signUpCheck.setOnClickListener{
-
+            if(idCheck == false){
+                Toast.makeText(this,"Please, ID Check First",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                getfile.appendText(signUpID.text.toString()+"\n")
+                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
         backBt.setOnClickListener{
             val intent = Intent(this,MainActivity::class.java)
@@ -35,6 +71,11 @@ class SignUpActivity:AppCompatActivity() {
                 signUpPW.transformationMethod = HideReturnsTransformationMethod()
             }
         }
+    }
+    fun File.readLines():List<String>{
+        var tmp = ArrayList<String>()
+        BufferedReader(InputStreamReader(FileInputStream(this))).forEachLine ({ tmp.add(it) })
+        return tmp
     }
 
 }
